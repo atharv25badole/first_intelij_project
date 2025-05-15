96235f3fd976e67905dae8ef7ca204ca1d449055
HP <badoleatharv@gmail.com>
2025-05-15 18:41:49 +0530

    Second Commit

---
+++ src/main/resources/main.java
@@ -0,0 +1,11 @@
+import jdk.internal.org.jline.terminal.TerminalBuilder;
+
+class Main{
+
+
+    public static void mian(String[] args){
+
+        System.out.println("Hello we are here to deploy this project");
+
+    }
+}
